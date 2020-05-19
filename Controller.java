59c0d520baa8b3dac9ff63042d9e6815251d2235
HanseLopez99 <halm29799@gmail.com>
2020-05-19 16:25:43 -0600

    Se creo Controller

---
+++ Controller.java
@@ -0,0 +1,13 @@
+/*Algoritmos y Estructura de Datos
+* Hoja de trabajo 10
+*Seccion 30
+*Autor: Hansel Lopez
+*Clase Main.java
+*Fecha: 19/05/2020*/
+
+public class Controller {
+    public void runProgram(){
+        Controller console = new Controller();
+		console.runProgram();
+    }
+}
