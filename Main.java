324adbe641e33acb78f894752b73e7840cbd75c2
HanseLopez99 <halm29799@gmail.com>
2020-05-19 16:25:10 -0600

    Se creo Main

---
+++ Main.java
@@ -0,0 +1,13 @@
+/*Algoritmos y Estructura de Datos
+* Hoja de trabajo 10
+*Seccion 30
+*Autor: Hansel Lopez
+*Clase Main.java
+*Fecha: 19/05/2020*/
+
+public class Main{
+    public static void main(String[] args){
+        Controller console = new Controller();
+		console.runProgram();
+    }
+}
