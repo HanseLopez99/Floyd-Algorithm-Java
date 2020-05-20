23391378dcd98d5849cccaac118020454544f43c
HanseLopez99 <halm29799@gmail.com>
2020-05-20 03:06:07 -0600

    Ligera correccion en Main
    - Se agrego throws IOException

--- Main.java
+++ Main.java
@@ -5,8 +5,10 @@
 *Clase Main.java
 *Fecha: 19/05/2020*/
 
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         Controller console = new Controller();
 		console.runProgram();
     }
