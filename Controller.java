21c0b1744b61aa5929eb1b666417fed3bcb62a93
HanseLopez99 <halm29799@gmail.com>
2020-05-20 03:51:24 -0600

    Se agrega lo necesario para la lectura del txt

--- Controller.java
+++ Controller.java
@@ -2,12 +2,51 @@
 * Hoja de trabajo 10
 *Seccion 30
 *Autor: Hansel Lopez
-*Clase Main.java
+*Clase Controller.java
 *Fecha: 19/05/2020*/
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Controller {
-    public void runProgram(){
-        Controller console = new Controller();
-		console.runProgram();
+
+    //Metodo que inicializa el programa
+    public void runProgram() throws IOException{
+        //Se declara variables necesarias para abrir el archivo de texto
+        ArrayList<GraphModel> graphArrayList = new ArrayList<>();
+        File fileTXT = new File ("guategrafo.txt");
+        FileReader reader = new FileReader(fileTXT);
+        BufferedReader bfReader = new BufferedReader(reader);
+        Scanner scanner = new Scanner(reader);
+        String line = "";
+
+        //Se declara variables del modelo y se crea una instancia
+        GraphModel graph = new GraphModel(); 
+        String origin;
+        String destination;
+        int distancia;
+        int cont = 0;
+
+        //Se lee el archivo de texto y se guarda en el arraylist graphArrayList
+        while (scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            
+            //Capturando origen, destino y distancia que contiene el archivo de texto
+            origin = line.substring(0, line.indexOf(" "));
+            line = line.substring(line.indexOf(" ") + 1, line.length());
+            destination = line.substring(0, line.indexOf(" "));
+            line = line.substring(line.indexOf(" ") + 1, line.length());  
+            distancia = Integer.parseInt(line.substring(0, line.length()));
+            cont++;
+            
+            //Agregando a ArrayList y cerrando BufferedReader
+            graphArrayList.add(new GraphModel(origin, destination, distancia)); 
+            reader.close();
+            bfReader.close();
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 
 public class Main{
     public static void main(String[] args) throws IOException{
+        //Instancia hacia el controlador
         Controller console = new Controller();
+        //Se inicializa el programa
 		console.runProgram();
     }
 }
