ee453c6bfd1d9ab7ba9303775d5e750a66a7a6e2
HanseLopez99 <halm29799@gmail.com>
2020-05-20 04:21:09 -0600

    Se agrego la estructura del menu principal

--- Controller.java
+++ Controller.java
@@ -31,6 +31,13 @@ public class Controller {
         int distancia;
         int cont = 0;
 
+        //Instancia del algoritmo de floyd
+        Floyd floyd = new Floyd();
+
+        //Se declara variables usadas en el menu
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
         //Se lee el archivo de texto y se guarda en el arraylist graphArrayList
         while (scanner.hasNextLine()) {
             line = scanner.nextLine();
@@ -47,6 +54,43 @@ public class Controller {
             graphArrayList.add(new GraphModel(origin, destination, distancia)); 
             reader.close();
             bfReader.close();
+        }
+
+        //Menu Principal
+        while (true) {
+            System.out.println("_______________________________________________________________________________________--");
+    	    System.out.println("    Lucha conta el Covid-19 / Rutas mas cortas hacia hospitales             ");
+            System.out.println("_______________________________________________________________________________________--\n");
+            System.out.println(" Por favor presione el numero segun la opcion elegida: \n");
+            System.out.println(" (1) Mostrar el valor de la ruta más corta entre ciudades por donde pasa esa ruta.");
+            System.out.println(" (2) Mostrar la ciudad que queda en el centro del grafo.");
+            System.out.println(" (3) Modificar grafo.");
+            System.out.println(" (4) Mostrar matriz de adyacencia.");
+            System.out.println("_______________________________________________________________________________________--");
+            System.out.println(" (5) Salir del programa");
+            System.out.println("_______________________________________________________________________________________--\n");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1: 
+                    //Opcion1
+                break;
+                case 2:
+                    //Opcion2
+                break;
+                case 3:
+                    //Opcion3
+                break;
+                case 4:
+                    //Opcion4
+                break;
+                case 5:
+                    //Opcion5
+                    break;
+                default:
+                    //Opcion default
+                    break;
+            }
         }
     }
 }
