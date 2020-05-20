2e04c22163508120bc7c54ba86d9959e195c8f08
HanseLopez99 <halm29799@gmail.com>
2020-05-20 05:41:26 -0600

    Se agregaron las opciones faltantes del menu principal
    -Se agrego opcion 3
    -Se agrego una cuarta opcion que muestra la matriz de adjacencia usando el metodo showMatrix

--- Controller.java
+++ Controller.java
@@ -40,6 +40,7 @@ public class Controller {
         Scanner sc = new Scanner(System.in);
         int opcion;
         String enter;
+        boolean salir = true;
 
         //Se lee el archivo de texto y se guarda en el arraylist graphArrayList
         while (scanner.hasNextLine()) {
@@ -60,7 +61,7 @@ public class Controller {
         }
 
         //Menu Principal
-        while (true) {
+        while (salir) {
             System.out.println("_______________________________________________________________________________________--");
     	    System.out.println("    Lucha conta el Covid-19 / Rutas mas cortas hacia hospitales             ");
             System.out.println("_______________________________________________________________________________________--\n");
@@ -92,12 +93,12 @@ public class Controller {
                     if(floyd.check(citiesArrayList,orgCity,desCity)){    
                         System.out.println(floyd.FloydAlgorithm(matrix, citiesArrayList, orgCity, desCity));
                         System.out.println("");
-                        System.out.println("Presione (0) para continuar... ");
+                        System.out.println(" (Presione (0) para continuar...) ");
                         enter = sc.next();
                         
                     }else{
                         System.out.println("Dato no existente \n");
-                        System.out.println("Presione (0) para continuar...");
+                        System.out.println(" (Presione (0) para continuar...)");
                         enter = sc.next();
                     }
                     break;
@@ -106,21 +107,69 @@ public class Controller {
                     citiesArrayList = graph.generateGraphArray(graphArrayList);
                     matrix = floyd.createMatrix(citiesArrayList, graphArrayList); //Se genera matriz
                     floyd.graphCenter(matrix); //Se usa el metodo para mostrar el centro del grafo
-                    System.out.println(" La ciudad que queda en el centro del grafo es: (" + citiesArrayList.get(floyd.graphCenter(matrix)) + ") \n");
-                    System.out.println("Presione (0) para continuar...");
+                    System.out.println(" La ciudad que queda en el centro del grafo es: (" + citiesArrayList.get(floyd.graphCenter(matrix)) + ")! \n");
+                    System.out.println(" (Presione (0) para continuar...)");
                     enter = sc.next();
                 break;
-                case 3:
-                    //Opcion3
-                break;
-                case 4:
-                    //Opcion4
+                case 3: //Opcion3
+                    String seleccion2 = "";
+                    System.out.println("\t (a) Insertar advertencia de interrupcion de trafico en la ruta entre dos ciudades.");
+                    System.out.println("\t (b) Establecer una nueva conexion entre ciudades, para una mejor movilizacion.");
+                    seleccion2 = sc.next();
+                    
+                    if("a".equals(seleccion2.toLowerCase())){
+                        System.out.println(" -Ingresar ciudad de origen: ");
+                        String ori = sc.next();
+                        System.out.println(" -Ingresar ciudad de destino: ");
+                        String dest = sc.next();
+                        
+                        boolean hubo = false;
+                        for(GraphModel c: graphArrayList){
+                            if((c.getOrigin().equals(ori)) && (c.getDestination().equals(dest))){
+                                graphArrayList.remove(c);
+                                hubo = true;
+                                System.out.println(" Se ha desplegado la advertencia de interrupcion correctamente!. \n");
+                                System.out.println(" (Presione (0) para continuar...)");
+                                enter = sc.next();
+                                break;
+                            }
+                        }
+                        if(hubo == false){
+                            System.out.println(" Lo siento dicha ciudad no pertenece al grafo o no existe ruta directa! \n");
+                            System.out.println(" (Presione (0) para continuar...)");
+                            enter = sc.next();
+                        }
+                    }else if("b".equals(seleccion2.toLowerCase())){
+                        System.out.println(" -Ingrese ciudad de origen:");
+                        String ori = sc.next();
+                        System.out.println(" -Ingrese ciudad de destino:");
+                        String dest = sc.next();
+                        System.out.println(" -Ingrese la distancia entre (" + ori + ") y (" + dest + "):");
+                        int dist = sc.nextInt();
+                        
+                        graphArrayList.add(new GraphModel(ori, dest, dist));
+                        System.out.println(" Se agregaron correctamente! \n");
+                        System.out.println(" (Presione (0) para continuar...)");
+                        enter = sc.next();
+                    }
+                    break;
+                case 4: //Opcion4   
+                    citiesArrayList.clear();
+                    citiesArrayList = graph.generateGraphArray(graphArrayList);
+                    matrix = floyd.createMatrix(citiesArrayList, graphArrayList);
+                    System.out.println(" La matriz de adyacencia es:");
+                    for(String s: citiesArrayList){
+                        System.out.print(s + " ");
+                    }
+                    System.out.println("");
+                    System.out.println(floyd.showMatrix(matrix));
+                    System.out.println("\n");
+                    System.out.println(" (Presione (0) para continuar...)");
+                    enter = sc.next();
                 break;
                 case 5:
-                    //Opcion5
-                    break;
+                    salir = false;
                 default:
-                    //Opcion default
                     break;
             }
         }
